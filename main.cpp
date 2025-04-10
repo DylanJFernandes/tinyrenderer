11e701f27ac4c47bb775ee7424cc1d879bcda30b
Dylan Fernandes <dfernandes4@student.cccd.edu>
2025-04-10 00:33:02 -0700

    Created VS Project and Implemented a Function for Drawing a Line Between Two Points

--- main.cpp
+++ main.cpp
@@ -7,6 +7,23 @@ constexpr TGAColor red     = {  0,   0, 255, 255};
 constexpr TGAColor blue    = {255, 128,  64, 255};
 constexpr TGAColor yellow  = {  0, 200, 255, 255};
 
+struct Point
+{
+    int x{ 0 }, y{ 0 };
+    Point(int xIn, int yIn) : x{ xIn }, y{ yIn } {}
+};
+void drawLine(const Point&  pointOne,  const Point& pointTwo, TGAImage& frameBuffer, const TGAColor& colorOfLine)
+{
+    float t{ 0.f };
+    while (t < 1.f)
+    {
+        int currentX = std::round(pointOne.x + t * (pointTwo.x - pointOne.x));
+        int currentY = std::round(pointOne.y + t * (pointTwo.y - pointOne.y));
+
+        frameBuffer.set(currentX, currentY, colorOfLine);
+        t += .01;
+    }
+}
 int main(int argc, char** argv) {
     constexpr int width  = 64;
     constexpr int height = 64;
@@ -16,10 +33,20 @@ int main(int argc, char** argv) {
     int bx = 12, by = 37;
     int cx = 62, cy = 53;
 
+    Point pointOne(ax, ay);
+    Point pointTwo(bx, by);
+    Point pointThree(cx, cy);
+
+    // Creates lines between points to form triangle
+    drawLine(pointOne, pointTwo, framebuffer, red);
+    drawLine(pointTwo, pointThree, framebuffer, green);
+    drawLine(pointThree, pointOne, framebuffer, blue);
+
+    // Sets intial points to white
     framebuffer.set(ax, ay, white);
     framebuffer.set(bx, by, white);
     framebuffer.set(cx, cy, white);
 
-    framebuffer.write_tga_file("framebuffer.tga");
+    framebuffer.write_tga_file("lineTest_01Percent.tga");
     return 0;
 }
